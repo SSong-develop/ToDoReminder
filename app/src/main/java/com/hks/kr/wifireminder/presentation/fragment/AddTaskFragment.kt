97d77b0c71570dc7ae727494f43e6743b04e8e0b
SSong-develop <thdgnsrl123@naver.com>
2021-04-23 14:55:48 +0900

    네비게이션 설정

---
+++ app/src/main/java/com/hks/kr/wifireminder/presentation/fragment/AddTaskFragment.kt
@@ -0,0 +1,22 @@
+package com.hks.kr.wifireminder.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.hks.kr.wifireminder.R
+import com.hks.kr.wifireminder.databinding.FragmentAddTaskBinding
+
+class AddTaskFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentAddTaskBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_add_task, container, false)
+
+        return binding.root
+    }
+}
