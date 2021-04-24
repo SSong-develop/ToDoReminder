07fe6f50ceec9528c150b092583bbf3caf0016f9
SSong-develop <thdgnsrl123@naver.com>
2021-04-24 22:48:55 +0900

    ViewModel application context

--- app/src/main/java/com/hks/kr/wifireminder/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/hks/kr/wifireminder/ui/viewmodel/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.hks.kr.wifireminder.ui.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.hks.kr.wifireminder.domain.repository.TaskRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
+    @ApplicationContext context : Context,
     private val taskRepository: TaskRepository
 ) : ViewModel() {
 
