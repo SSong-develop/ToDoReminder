4d48a6108aba7a2b78fa1c88061f77b59dad3573
SSong-develop <thdgnsrl123@naver.com>
2021-05-01 20:20:37 +0900

    DataStore with Flow

--- app/src/main/java/com/hks/kr/wifireminder/datastore/PrefsStoreImpl.kt
+++ app/src/main/java/com/hks/kr/wifireminder/datastore/PrefsStoreImpl.kt
@@ -1,18 +1,19 @@
 package com.hks.kr.wifireminder.datastore
 
 import android.content.Context
-import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
-import androidx.work.impl.model.Preference
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
-class PrefsStoreImpl @Inject constructor(@ApplicationContext context : Context) : PrefsStore {
+class PrefsStoreImpl @Inject constructor(@ApplicationContext context: Context) : PrefsStore {
     private val dataStore = context.createDataStore(
         name = STORE_NAME
     )
@@ -22,11 +23,15 @@ class PrefsStoreImpl @Inject constructor(@ApplicationContext context : Context) 
         pref[IS_ALREADY_DONE_IT_BEFORE] == true
     }.getOrDefault(false)
 
-    override suspend fun isAlreadyWorkedWithFlow(): Flow<Boolean> = dataStore.data.map { prefs ->
-        prefs[IS_ALREADY_DONE_IT_BEFORE] == true
-    }
+    override suspend fun isAlreadyWorkedWithFlow(): Flow<Boolean> =
+        dataStore.data.catch { exception ->
+            if (exception is IOException) emit(emptyPreferences())
+            else throw exception
+        }.map { prefs ->
+            prefs[IS_ALREADY_DONE_IT_BEFORE] == true
+        }
 
-    override suspend fun runOnlyOnce(){
+    override suspend fun runOnlyOnce() {
         runCatching {
             dataStore.edit { pref ->
                 pref[IS_ALREADY_DONE_IT_BEFORE] = true
