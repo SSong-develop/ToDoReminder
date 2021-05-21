159f3b22ec12531f5267d8a895f58e34c5785f94
SSong-develop <thdgnsrl123@naver.com>
2021-05-21 12:09:51 +0900

    DataBinding Component를 생명주기와 함께 사용하는 방법

--- app/src/main/java/com/hks/kr/wifireminder/binding/AppViewBindingImpl.kt
+++ app/src/main/java/com/hks/kr/wifireminder/binding/AppViewBindingImpl.kt
@@ -1,11 +1,15 @@
 package com.hks.kr.wifireminder.binding
 
 import android.widget.TextView
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 
-class AppViewBindingImpl(private val scope: LifecycleCoroutineScope) : AppViewBinding {
+class AppViewBindingImpl(private val scope: LifecycleCoroutineScope) : AppViewBinding , LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     override fun setText(view: TextView, stateFlow: StateFlow<String>) {
         scope.launchWhenStarted {
             stateFlow.collect {
