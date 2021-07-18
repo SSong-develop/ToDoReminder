479be2cebdc861f98918d336bb18d838a6609471
SSong-develop <thdgnsrl123@naver.com>
2021-07-18 23:35:49 +0900

    delete Calendar

--- app/src/main/java/com/hks/kr/wifireminder/view/calendar/ui/WeekHolder.kt
+++
@@ -1,4 +0,0 @@
-package com.hks.kr.wifireminder.view.calendar.ui
-
-internal class WeekHolder(private val dayHolders : List<DayHolder>) {
-}
--- app/src/main/java/com/hks/kr/wifireminder/view/calendar/util/Size.kt
+++
@@ -1,5 +0,0 @@
-package com.hks.kr.wifireminder.view.calendar.util
-
-import androidx.annotation.Px
-
-data class Size(@Px val width: Int, @Px val height: Int)
