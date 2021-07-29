2e86694598b483543d464b00b94f983676375a7a
SSong-develop <thdgnsrl123@naver.com>
2021-07-29 18:10:54 +0900

    make calendar

--- app/src/main/java/com/hks/kr/wifireminder/view/calendarPicker/viewholder/CalendarViewHolder.kt
+++ app/src/main/java/com/hks/kr/wifireminder/view/calendarPicker/viewholder/CalendarViewHolder.kt
@@ -2,9 +2,12 @@ package com.hks.kr.wifireminder.view.calendarPicker.viewholder
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.hks.kr.wifireminder.R
 import com.hks.kr.wifireminder.view.calendarPicker.entity.CalendarEntity
+import com.hks.kr.wifireminder.view.calendarPicker.entity.DateState
+import com.hks.kr.wifireminder.view.calendarPicker.entity.SelectionType
 
 abstract class BaseCalendarViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     abstract fun onBind(item : CalendarEntity , actionListener : (CalendarEntity,Int) -> Unit)
@@ -29,9 +32,55 @@ class DayViewHolder(private val view : View) : BaseCalendarViewHolder(view){
     override fun onBind(item: CalendarEntity, actionListener: (CalendarEntity, Int) -> Unit) {
         if(item is CalendarEntity.Day){
             name.text = item.label
+            when(item.selection) {
+                SelectionType.START -> {
+
+                }
+                SelectionType.END -> {
+
+                }
+                SelectionType.BETWEEN -> {
+
+                }
+                SelectionType.NONE -> {
+
+                }
+            }
         }
     }
 
+    private fun getFontColor(item : CalendarEntity.Day) : Int {
+        return if(item.selection == SelectionType.START || item.selection == SelectionType.END) {
+            ContextCompat.getColor(itemView.context,R.color.calendar_day_selected_font_color)
+        } else if(item.selection == SelectionType.BETWEEN && item.state != DateState.WEEKEND) {
+            ContextCompat.getColor(itemView.context,R.color.teal_200)
+        } else {
+            val color = when(item.state){
+                DateState.DISABLED -> R.color.gray
+                DateState.WEEKEND -> R.color.red
+                else -> R.color.black
+            }
+            ContextCompat.getColor(itemView.context,color)
+        }
+    }
+
+    private fun View.select() {
+        val drawable = ContextCompat.getDrawable(context,R.drawable.bg_calendar_select_day)
+        background = drawable
+    }
+
+    private fun View.deSelect() {
+        background = null
+    }
+
+    private fun View.dehighlight() {
+        val color = ContextCompat.getColor(context,R.color.calendar_day_unselected_bg_color)
+        setBackgroundColor(color)
+    }
+
+    private fun View.highlight() {
+
+    }
 }
 class CalendarViewHolder {
 }
